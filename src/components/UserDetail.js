566f36c8ecdb3a0cf6d2df0301f9a16ac426c706
Sachin C <sachinc@Sachins-MacBook-Pro.local>
2021-04-11 07:56:21 +0530

    add proptypes

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -1,9 +1,11 @@
-const UserDetail = ({ user, i }) => {
+import PropTypes from "prop-types";
+
+const UserDetail = ({ user, index }) => {
 
     return (
         <>
             <tr className="user-detail-row">
-                <td className="user-detail-index border px-8 py-4">{i}</td>
+                <td className="user-detail-index border px-8 py-4">{index}</td>
                 <td className="user-detail-avatar border px-8 py-4">
                     <img className="rounded-full" src={user.avatar} alt="" />
                 </td>
@@ -13,5 +15,20 @@ const UserDetail = ({ user, i }) => {
         </>
     )
 }
+
+UserDetail.defultProps = {
+    user: null,
+    index: 0,
+}
+
+UserDetail.PropTypes = {
+    user: PropTypes.shape({
+        avatar: PropTypes.string,
+        name: PropTypes.string,
+        credits: PropTypes.number,
+    }),
+    index: PropTypes.number
+}
+
 
 export default UserDetail;
